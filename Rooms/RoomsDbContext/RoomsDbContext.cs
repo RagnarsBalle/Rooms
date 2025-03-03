cdb64e801223b4631d8c7efe6c0acc06327751a4
Patrik Lindqvist <patrik.lindqvist@student.hv.se>
2025-03-03 09:44:49 +0100

    Add project files.

---
+++ Rooms/RoomsDbContext/RoomsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Rooms.RoomsDbContext
+{
+    public class RoomsDbContext : DbContext
+    {
+        public RoomsDbContext(DbContextOptions<RoomsDbContext> options) : base(options) { }
+
+        public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
